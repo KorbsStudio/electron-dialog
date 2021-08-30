6fbac84829512ad92cb90d7541475eabc91f419d
Korbs <KorbsStudio@OnMail.com>
2021-08-29 20:29:56 -0400

    Remove remote process and use ipcMain

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, dialog, BrowserWindow} = require('electron')
+const {app, dialog, BrowserWindow, ipcMain, ipcRenderer} = require('electron')
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
@@ -12,6 +12,46 @@ function createWindow () {
     }
   })
   mainWindow.loadFile('index.html')
+  ipcMain.on('dialogMessage', () => {dialogMessage()})
+  ipcMain.on('dialogQuestion', () => {dialogQuestion()})
+  ipcMain.on('dialogError', () => {dialogError()})
 }
 
-app.whenReady().then(() => {createWindow()})
+function dialogMessage() {
+  const options = {
+    type: 'info',
+    title: 'Dialog',
+    message: 'Title of dialog.',
+    detail: 'Details for the message dialog.',
+  };
+  
+  dialog.showMessageBox(null, options, (response, checkboxChecked) => {});
+}
+
+function dialogError() {
+  const options = {
+    type: 'error',
+    title: 'Dialog',
+    message: 'Title of error.',
+    detail: 'Details for the error dialog.',
+  };
+  
+  dialog.showMessageBox(null, options, (response, checkboxChecked) => {});
+}
+
+function dialogQuestion() {
+  const options = {
+    type: 'question',
+    buttons: ['Cancel', 'Option One', 'Option Two', 'Option Three'],
+    defaultId: 2,
+    title: 'Dialog',
+    message: 'Do you want to continue?',
+    detail: 'Choose any of the options below or cancel.',
+    checkboxLabel: 'This is a checkbox',
+    checkboxChecked: true,
+  };
+  
+  dialog.showMessageBox(null, options, (response, checkboxChecked) => {});
+}
+ 
+app.whenReady().then(() => {createWindow();})
